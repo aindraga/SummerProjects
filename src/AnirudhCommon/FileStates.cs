997e47b723a7178b5253b0345cfa6c1159d6e6d8
Sri Annu <SriAnnu@outlook.com>
2020-07-15 10:55:33 -0700

    New class for serialization

---
+++ src/AnirudhCommon/FileStates.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnirudhCommon
+{
+    public class FileStates
+    {
+        public string copiedFiles { get; set; }
+
+        public static bool Serialize(FileStates filestate, string jsonPath)
+        {
+            string serializedObj = JsonConvert.SerializeObject(filestate);
+            File.WriteAllText(jsonPath, serializedObj);
+            return true;
+        }
+        public static bool DeSerialize(string jsonPath)
+        {
+            string allContent = File.ReadAllText(jsonPath);
+            List<string> filesCopied = JsonConvert.DeserializeObject<List<string>>(allContent);
+            return true;
+        }
+    }
+}
