32c13cfff7187506f34c706788e73c0d69232aa4
Sri Annu <SriAnnu@outlook.com>
2020-07-16 18:21:06 -0700

    Old name was Program.cs and it only calls methods to run process now

---
+++ src/FileCopy/Runner.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------------------
+// <copyright company="Anirudh Indraganti" file="Runner.cs">
+//      Copyright ©️ Anirudh Indraganti. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------------------
+namespace FileCopyNetF
+{
+    using AnirudhCommon;
+
+    public class Runner
+    {
+        public static void Main(string[] args)
+        {
+            if (Validation.InputValidation(args))
+            {
+                Copying.CopyAndMove(args[0], args[1]);
+            }
+        }
+    }
+}
