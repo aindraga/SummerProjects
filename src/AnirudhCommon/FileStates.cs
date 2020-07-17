7673979260b3d08c081a986376a4bd51a1aa4124
Sri Annu <SriAnnu@outlook.com>
2020-07-16 18:17:14 -0700

    New class with style checks and modifications

--- src/AnirudhCommon/FileStates.cs
+++ src/AnirudhCommon/FileStates.cs
@@ -1,27 +1,59 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿//-----------------------------------------------------------------------------------
+// <copyright company="Anirudh Indraganti" file="FileStates.cs">
+//      Copyright ©️ Anirudh Indraganti. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------------------
 namespace AnirudhCommon
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Class that maintains state of files that were copied. Class contains both serialization and deserialization methods
+    /// </summary>
     public class FileStates
     {
-        public string copiedFiles { get; set; }
-
-        public static bool Serialize(FileStates filestate, string jsonPath)
+        /// <summary>
+        /// Initializes a new instance of the FileStates class
+        /// </summary>
+        public FileStates()
         {
-            string serializedObj = JsonConvert.SerializeObject(filestate);
+            this.CopiedFiles = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Gets the files that were copied in run time process
+        /// </summary>
+        public HashSet<string> CopiedFiles { get; set; }
+
+        /// <summary>
+        /// Serializes the list of copied files 
+        /// </summary>
+        /// <param name="jsonPath"> the file path where the file states are stored </param>
+        /// <returns> a boolean of whether it the process was successful </returns>
+        public bool Serialize(string jsonPath)
+        {
+            string serializedObj = JsonConvert.SerializeObject(this.CopiedFiles);
             File.WriteAllText(jsonPath, serializedObj);
             return true;
         }
-        public static bool DeSerialize(string jsonPath)
+
+        /// <summary>
+        /// Deserializes the list to be referenced in Copying.cs during the copy process
+        /// </summary>
+        /// <param name="jsonPath"> the file path where the file states are stored </param>
+        /// <returns> a boolean of whether the process was successful </returns>
+        public bool DeSerialize(string jsonPath)
         {
+            if (!File.Exists(jsonPath))
+            {
+                return false;
+            }
+
             string allContent = File.ReadAllText(jsonPath);
-            List<string> filesCopied = JsonConvert.DeserializeObject<List<string>>(allContent);
+            this.CopiedFiles = JsonConvert.DeserializeObject<HashSet<string>>(allContent);
             return true;
         }
     }
