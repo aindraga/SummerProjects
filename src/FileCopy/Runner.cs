83971c3b94703636aec479c37a1e55e0d4657d91
Sri Annu <SriAnnu@outlook.com>
2020-07-17 16:45:07 -0700

    New documentation added that cancels all StyleCop errors

--- src/FileCopy/Runner.cs
+++ src/FileCopy/Runner.cs
@@ -7,8 +7,15 @@ namespace FileCopyNetF
 {
     using AnirudhCommon;
 
+    /// <summary>
+    /// Runs the code for copying files
+    /// </summary>
     public class Runner
     {
+        /// <summary>
+        /// Main method that calls the methods needed to run
+        /// </summary>
+        /// <param name="args"> Command line arguments </param>
         public static void Main(string[] args)
         {
             if (Validation.InputValidation(args))
