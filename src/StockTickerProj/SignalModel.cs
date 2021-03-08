1f6bbe3367a0f972efca7db27c04a5a794ddcec0
Sri Annu <SriAnnu@outlook.com>
2021-03-07 22:52:08 -0800

    New Model replacement

---
+++ src/StockTickerProj/SignalModel.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------------------
+// <copyright company="Anirudh Indraganti" file="TickerModel.cs">
+//      Copyright ©️ Anirudh Indraganti. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------------------
+
+namespace CallRestAPI
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json.Linq;
+    /// <summary>
+    /// Class that creates a model for the called data to be stored in.
+    /// </summary>
+    public class SignalModel
+    {
+        /// <summary>
+        /// Gets or sets a string of the buy/sell signal of multiple indicators 
+        /// </summary>
+        public JObject technicalAnalysis { get; set; }
+    }
+}
