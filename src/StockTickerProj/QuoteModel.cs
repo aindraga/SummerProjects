54845988867447991a199ce9b3a0c9a4654b997c
Sri Annu <SriAnnu@outlook.com>
2021-03-07 22:51:18 -0800

    New model for extra API call

---
+++ src/StockTickerProj/QuoteModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CallRestAPI
+{
+    /// <summary>
+    /// Class that creates a model that contains data from quote API call.
+    /// </summary>
+    public class QuoteModel
+    {
+        /// <summary>
+        /// Gets or sets the current price of the stock
+        /// </summary>
+        public double c { get; set; }
+    }
+}
